655475acf3c63c56bba6a90c34bea6dae6942080
Saiki Tanabe <saiki.tanabe@7scales.net>
2015-06-23 09:07:01 +0300

    fix: touch menu links don't work

--- js/header.js
+++ js/header.js
@@ -1,32 +1,57 @@
+function isDescendant(parent, child) {
+	var node = child.parentNode
+	while (node != null) {
+	  if (node == parent) {
+      return true
+	  }
+	  node = node.parentNode
+	}
+	return false
+}
+
+function isOutsideElement(elem, targetElem) {
+	return elem != targetElem &&
+				 !isDescendant(elem, targetElem)
+}
+
 (function() {
 	var integrations = document.getElementById('integrations')
 	var showing = false
 
 	function showHide(e) {
-		if (!showing) {
-			e.preventDefault()
-			e.stopPropagation()
-		}
+		e.preventDefault()
+		e.stopPropagation()
 
 		var menu = integrations.parentNode.querySelector('ul')
 		if (showing) {
-			menu.style.display = 'none'
+			animate({
+				el: menu,
+				scaleX: [1, 0.5],
+				scaleY: [1, 0.5],
+				easing: 'easeInOutQuart',
+				duration: 200,
+				complete: function() {
+					menu.style.display = 'none'
+				}
+			})
 		} else {
 			menu.style.display = 'block'
+			animate({
+				el: menu,
+				scaleX: [1.1, 1],
+				scaleY: [1.1, 1],
+				easing: 'easeInOutQuart',
+				duration: 200
+			})
 		}
-		animate({
-			el: menu,
-			scaleX: [1.1, 1],
-			scaleY: [1.1, 1],
-			easing: 'easeInOutQuart',
-			duration: 200
-		})
 		showing = !showing 
 	}
 
 	function hideOnly(e) {
-		if (showing) {
-			showHide(e)
+		if (showing && isOutsideElement(integrations, e.target)) {
+			setTimeout(function() {
+				showHide(e)
+			}, 0)
 		}
 	}
 
